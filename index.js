f1f4fbe106b97abbcce2ca1ad6b2dec1a0b64956
Mike-Do <michael_donoso+cs1300@brown.edu>
2022-12-08 17:26:25 -0500

    Add update

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ document.querySelectorAll('.my-lightbox-toggle').forEach((el) => el.addEventList
 	const lightbox = new Lightbox(el, options);
 	lightbox.show();
 }));
+
+
+// CITATION: https://codepen.io/MiguelCodes/pen/XWdedmw Gradient Scroll Bar
+const progressBar = document.querySelector("#progressBar");
+let totalPageHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = () => {
+  let newProgressHeight = (window.pageYOffset / totalPageHeight) * 100;
+  progressBar.style.height = `${newProgressHeight}%`;
+  progressBar.style.opacity = `${newProgressHeight}%`;
+};
+
